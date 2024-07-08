a179fa0bd7ff3fade32eabc1d5fc3cfcb1c67116
Axenus <platon-gamer@mail.ru>
2024-07-08 11:57:11 +0800

    Add project files.

---
+++ Arrays/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random random = new Random(0);
+            //Console.WriteLine("Set array`s size: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int[] arr = new int[n];
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    arr[i] = random.Next(10);
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write(arr[i] + "\t");
+            //}
+            //Console.WriteLine();
+
+            //foreach (int i in arr)
+            //{
+            //    Console.Write(i + "\t");
+            //}
+            //Console.WriteLine();
+
+            //// 2d arrays
+
+            //int rows, cols;
+            //Console.WriteLine("enter number of Rows: "); rows = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("enter number of Cols: "); cols = Convert.ToInt32(Console.ReadLine());
+
+            //int[,] i_arr_2 = new int[rows, cols];
+            //Console.WriteLine($"Number of demensions: {i_arr_2.Rank}");
+            //for (int i = 0; i < i_arr_2.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < i_arr_2.GetLength(1); j++)
+            //    {
+            //        i_arr_2[i, j] = random.Next(0, 200);
+            //        Console.Write(i_arr_2[i, j] + "\t");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //foreach (int i in i_arr_2)
+            //{
+            //    Console.Write(i + "\t");
+            //}
+            //Console.WriteLine();
+
+            //jagged arrays
+
+            int row = 5;
+            int[][] i_arr_j = new int[][]
+                {
+                    new int[] { 1, 2, 3 },
+                    new int[] { 4, 5, 6, 7, 8 },
+                    new int[] { 9, 10, 11, 12, 13, 14, 15, 16 },
+                    new int[] { 17, 18, 19, 20, 21, 22 }
+                };
+            for (int i = 0; i < i_arr_j.Length; i++)
+            {
+                for (int j = 0; j < i_arr_j[i].Length; j++)
+                {
+                    Console.Write(i_arr_j[i][j] + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ dz1/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose array type: \n1 - 1d array\n2 - 2d array\n3 - jagged array: ");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            int Sum = 0, Avg = 0, min = 0, max = 0;
+            Random rnd = new Random();
+            switch (choose)
+            {
+                case 1:
+                    Console.WriteLine("Set array length");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int[] arr = new int[n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        arr[i] = rnd.Next(0, 300);
+                        Console.Write(arr[i] + "\t");
+                    }
+                    min = arr[0];
+                    foreach (int i in arr)
+                    {
+                        Sum += i;
+                        if (min > i)
+                        { min = i; }
+                        if (max < i)
+                        { max = i; }
+                    }
+                    Avg = Sum / n;
+                    Console.WriteLine();
+                    Console.WriteLine($"Summ = {Sum}; Average = {Avg}; Min = {min}; Max = {max}");
+                    Console.WriteLine();
+                    break;
+                case 2:
+                    int Rows, Cols;
+                    Console.WriteLine("Set array Rows: ");
+                    Rows = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Set array Cols: ");
+                    Cols = Convert.ToInt32(Console.ReadLine());
+                    int[,] arr_2d = new int[Rows, Cols];
+                    for (int i = 0; i < Rows; i++)
+                    {
+                        for (int j = 0; j < Cols; j++)
+                        {
+                            arr_2d[i, j] = rnd.Next(0, 300);
+                            Console.Write(arr_2d[i, j] + "\t");
+                            Sum += arr_2d[i, j];
+                        }
+                        Console.WriteLine();
+                    }
+                    foreach (int i in arr_2d)
+                    {
+                        if (min > i)
+                        { min = i; }
+                        if (max < i)
+                        { max = i; }
+                    }
+                    Avg = Sum / (Rows * Cols);
+                    Console.WriteLine();
+                    Console.WriteLine($"Summ = {Sum}; Average = {Avg}; Min = {min}; Max = {max}");
+                    Console.WriteLine();
+                    break;
+                case 3:
+                    int Rows_jag;
+                    int elements = 0;
+                    Console.WriteLine("Set jagged array Rows: ");
+                    Rows_jag = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    int[][] arr_jag = new int[Rows_jag][];
+                    for (int i = 0; i < Rows_jag; i++)
+                        {
+                            int size = rnd.Next(1,10);
+                            arr_jag[i] = new int[size];
+                        }
+                    for (int i = 0; i < Rows_jag; i++)
+                    {
+                        for (int j = 0; j < arr_jag[i].Length; j++)
+                        {
+                            arr_jag[i][j] = rnd.Next(0,300);
+                            Console.Write(arr_jag[i][j] + "\t");
+                            elements++;
+                            Sum += arr_jag[i][j];
+                        }
+                        Console.WriteLine();
+                    }
+                    min = arr_jag[0][0];
+                    for (int i = 0; i < Rows_jag; i++)
+                    {
+                        for (int j = 0; j < arr_jag[i].Length; j++)
+                        {
+                            if (min > arr_jag[i][j])
+                            { min = arr_jag[i][j]; }
+                            if (max < arr_jag[i][j])
+                            { max = arr_jag[i][j]; }
+                        }
+                    }
+                    Avg = Sum / elements;
+                    Console.WriteLine();
+                    Console.WriteLine($"Summ = {Sum}; Average = {Avg}; Min = {min}; Max = {max}");
+                    Console.WriteLine();
+                    break;
+            }
+        }
+    }
+}
